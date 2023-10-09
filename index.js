7eb2ae93e45d42d615e8ecf2fa2d6fc8b0ebf464
bony_musau <boniface.musau@moringaschool.com>
2023-10-09 07:46:23 +0300

    Array lab assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution here!
+const cats =["Milo","Otis","Garfield"];
+console.log(cats.length)
+console.log(cats)
+const copies=cats.slice();
+const same=cats.slice();
+const copy=cats.slice();
+const copyofcats=[...cats];
+cats.push("Ralph");
+console.log(cats)
+
+copyofcats.unshift("Bob");
+console.log(copyofcats);
+
+copy.pop();
+console.log(copy);
+
+same.shift()
+console.log(same)
+
+
+const newcats=[...cats.slice(3,0),
+ 
+]
